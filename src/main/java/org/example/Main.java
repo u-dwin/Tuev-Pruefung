bae57c0131318826d26ee9e65ec32598db939600
daniel <daniel.windel@unitybox.de>
2023-01-27 12:49:38 +0100

    Changes by gadmel

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Car> carPark = initialiseCarPark();
+
+        System.out.println("There are following cars in the given car park: ");
+        carPark.forEach(car -> System.out.println(car.toString()));
     }
+
+    public static List<Car> initialiseCarPark() {
+        Car mercedes = new Car();
+        Car volkswagen = new Car(4, 3, true, true);
+        Car kamaz = new Car(10, 2, true, true);
+
+       // List<Car> carPark = new ArrayList<>(List.of(mercedes, volkswagen, kamaz)) ;
+
+        return new ArrayList<>(List.of(mercedes, volkswagen, kamaz));
+    }
+
 }
